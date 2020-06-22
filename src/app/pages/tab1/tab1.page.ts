8d27bf21123909d9de023c86c25bcd7b72c16fda
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-06-21 19:40:46 -0500

    Guardando la información en el localStorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 import { DeseosService } from '../../services/deseos.service';
 
@@ -9,9 +10,47 @@ import { DeseosService } from '../../services/deseos.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor(public deseoService: DeseosService, private router: Router) {}
+  constructor(
+    public deseoService: DeseosService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
-  agregarLista() {
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista() {
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre De La Lista',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          },
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            if (data.titulo.length === 0) {
+              return;
+            }
+
+            /* Tengo Que Crear La Lista */
+            this.deseoService.crearLista(data.titulo);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,9 +8,29 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-    const lista1 = new Lista('Recolectar Piedras Del Infinito');
-    const lista2 = new Lista('Héroes A Desaparecer');
+    this.obtenerStorage();
 
-    this.listas.push(lista1, lista2);
+    /* LISTA DE EJEMPLO PERO YA ESTOY UTILIZANDO EL LOCALSTORAGE */
+    /* const lista1 = new Lista('Recolectar Piedras Del Infinito');
+    const lista2 = new Lista('Héroes A Desaparecer');
+    this.listas.push(lista1, lista2); */
+  }
+
+  crearLista(titulo: string) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  obtenerStorage() {
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
   }
 }
